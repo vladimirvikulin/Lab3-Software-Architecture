c4f53b3efaefe898dc60c767b401f8aed84829ed
danil2205 <04danil@gmail.com>
2023-04-29 20:33:36 +0300

    added: drawing cross

--- ui/window.go
+++ ui/window.go
@@ -135,8 +135,9 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
-
+    pw.w.Fill(image.Rect(200, 325, 600, 475), color.RGBA{R: 255, G: 255, B: 0, A: 1}, draw.Src)
+    pw.w.Fill(image.Rect(325, 200, 474, 600), color.RGBA{R: 255, G: 255, B: 0, A: 1}, draw.Src)
+    
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
 		pw.w.Fill(br, color.White, draw.Src)
